7a7a6b8673236623421812b02b77e01578b88da3
Aeneas Wiener <aeneas.wiener@gmail.com>
2014-01-04 22:00:05 +0000

    clone cartodb from fork due to incorrect version in Gemfile

--- setup/fetch-sources.sh
+++ setup/fetch-sources.sh
@@ -2,6 +2,6 @@
 
 cd /usr/local/src
 
-git clone --recursive https://github.com/CartoDB/cartodb.git
+git clone --recursive https://github.com/aeneaswiener/cartodb.git
 git clone https://github.com/CartoDB/CartoDB-SQL-API.git
 git clone https://github.com/CartoDB/Windshaft-cartodb.git
