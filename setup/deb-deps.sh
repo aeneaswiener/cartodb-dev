1b0adf896ea7addee86b22e1f0d7b0212be1ecbf
Aeneas Wiener <aeneas.wiener@gmail.com>
2014-01-04 22:00:56 +0000

    install vim and chrome for local testing

--- setup/deb-deps.sh
+++ setup/deb-deps.sh
@@ -24,3 +24,8 @@ apt-get install -y make unp zip libgeos-c1 libgeos-dev gdal-bin libgdal1-dev lib
   postgresql-plpython-9.1 ruby1.9.1 ruby1.9.1-dev nodejs npm redis-server libmapnik-dev \
   mapnik-utils python-mapnik git python-setuptools python-gdal gdal-bin libgdal1-dev
 
+# Install vim and chrome
+cd ~ 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+sudo dpkg -i google-chrome*; sudo apt-get -f install
+sudo apt-get install vim
