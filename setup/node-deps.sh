7844aaf977666059461ff9b0640c7b20d6724254
Aeneas Wiener <aeneas.wiener@gmail.com>
2014-01-04 21:58:53 +0000

    configure Windshaft to expect mapnik v2.1.0

--- setup/node-deps.sh
+++ setup/node-deps.sh
@@ -2,9 +2,10 @@
 
 cd /usr/local/src/CartoDB-SQL-API
 git checkout master
+./configure
 npm install
 
 cd /usr/local/src/Windshaft-cartodb
 git checkout master
+./configure --with-mapnik-version='2.1.0'
 npm install
-
