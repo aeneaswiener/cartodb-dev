1bb767eb94271e91891c659ef5a9234fe615362d
Aeneas Wiener <aeneas.wiener@gmail.com>
2014-01-08 13:07:55 +0000

    use forked repo of SQL API

--- setup/fetch-sources.sh
+++ setup/fetch-sources.sh
@@ -3,5 +3,6 @@
 cd /usr/local/src
 
 git clone --recursive https://github.com/aeneaswiener/cartodb.git
-git clone https://github.com/CartoDB/CartoDB-SQL-API.git
+git clone https://github.com/aeneaswiener/CartoDB-SQL-API.git
 git clone https://github.com/aeneaswiener/Windshaft-cartodb.git
+git clone https://github.com/aeneaswiener/cartodb-dev.git
